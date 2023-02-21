d0297d92386ebecf63b875f94d4356c3f25638c5
yiqiaoqiao <yqq0505@gmail.com>
2023-02-20 21:14:22 -0800

    Implement main page and link to product elements
    Main page contains four products (in an array) and each with
    its own image, name, price, add to cart button, category, stock amount,
    etc. The main page contains routes to all products, and the header main
    logo routes to main page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <h1>eBruin</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">eBruin</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'First Product',
+      slug: '1nd-p',
+      category: '1st category',
+      image: '/images/p1.jpeg', // 679px * 829px
+      price: 120,
+      countInStock: 10,
+      brand: '1st Brand',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'This is the Description for the first product',
+    },
+    {
+      name: 'Second Product',
+      slug: '2st-p',
+      category: '2st category',
+      image: '/images/p2.jpeg',
+      price: 100,
+      countInStock: 100,
+      brand: '2st Brand',
+      rating: 4.0,
+      numReviews: 15,
+      description: 'This is the Description for the 2nd product',
+    },
+    {
+      name: 'Third Product',
+      slug: '3rd-p',
+      category: '3st category',
+      image: '/images/p3.jpeg',
+      price: 80,
+      countInStock: 6,
+      brand: '3st Brand',
+      rating: 4.5,
+      numReviews: 50,
+      description: 'This is the Description for the 3rd product',
+    },
+    {
+      name: 'Fourth Product',
+      slug: '4rd-p',
+      category: '4st category',
+      image: '/images/p4.jpeg',
+      price: 50,
+      countInStock: 90,
+      brand: '4st Brand',
+      rating: 5.0,
+      numReviews: 30,
+      description: 'This is the Description for the 4rd product',
+    },
+  ],
+};
+
+export default data;
