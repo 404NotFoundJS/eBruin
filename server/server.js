c2825248943c4d5bd91acc2cbeb9bbbdec3cc18c
Jeremy S <jeremysjj666@gmail.com>
2023-02-21 12:06:30 -0800

    Add .gitignore and code cleanup
    Added .gitignore for node.
    Remove node_modules from repository.
    Code cleanup.

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get("/", (req, res) => res.send("Hello world!"));
 app.listen(port, () => console.log(`Server running on port ${port}`));
