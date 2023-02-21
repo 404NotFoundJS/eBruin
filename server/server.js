b65dc46831be2cc00ce21f53c811d5e762697a11
HarryPlier <lhuang02@g.ucla.edu>
2023-02-21 01:00:20 -0800

    Basic Express backend with MongoDB
    Added very barebone backend structure. Created a MongoDB cluster and
    connected it with the project. All changes are within the newly
    created folder /server.

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Hello world!'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
