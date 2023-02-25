1c2a96937108af20f7355e046cdb5ce3ef0e59e0
lintaocui <cuilintao1@163.com>
2023-02-24 19:14:08 -0800

    Implemented signin page

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
   const { state } = useContext(Store);
@@ -38,9 +39,11 @@ function App() {
         <main>
           <Container className="mt-3">
             <Routes>
+              <Route path="/" element={<SigninScreen />} />
+              <Route path="/home" element={<HomeScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
-              <Route path="/" element={<HomeScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>
