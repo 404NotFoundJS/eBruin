41c5fbc8762d487937628cbf6464d8b58afb3445
lintaocui <cuilintao1@163.com>
2023-02-24 20:09:50 -0800

    Connected to MongoDB on server side

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # eBruin
 UCLA CS 35L 23W Group Project
+
+```mongodb password
+username: ebruin
+password: ebruin
+create .env in server side and write
+MONGODB_URI=mongodb+srv://ebruin:<ebruin>@cluster0.ehynfvc.mongodb.net/ebruin?retryWrites=true&w=majority
+```
+
+```npm
+npm install mongoose
+npm install dotenv
+```
+
--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 //const express = require('express');
 //const bodyParser = require('body-parser');
 //const connectDB = require('./config/db');
 //const { default: data } = require('./data.js');
+dotenv.config();
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URI).
+then(()=>{console.log('Connected to MongoDB')}).catch(err =>{console.log(err.message)});
 
 const app = express();
 
