443a19bb1bc1fd6161627ecbeefb5e6b0367e0a1
lintaocui <cuilintao1@163.com>
2023-02-25 17:07:22 -0800

    signin front end connected to backend

--- client/src/screens/SigninScreen.js
+++ client/src/screens/SigninScreen.js
@@ -3,25 +3,48 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
+import {  useState } from 'react';
+
+import Axios from 'axios';
+
 
 export default function SigninScreen() {
     const { search } = useLocation();
     const redireactURL = new URLSearchParams(search).get('redirect');
     const redirect = redireactURL ? redireactURL : '/';
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await Axios.post('/api/users/signin', {
+                email,
+                password,
+            });
+            console.log(data);
+
+        } catch (err) {
+
+        }
+    };
+
+
+
     return (
         <Container className="container">
             <Helmet>
                 <title>Sign In</title>
             </Helmet>
             <h1 className="my-3">Sign In</h1>
-            <Form>
+            <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-3" controlId="email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" required />
+                    <Form.Control type="email" required onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" required />
+                    <Form.Control type="password" onChange={(e) => setPassword(e.target.value)}/>
                 </Form.Group>
                 <div className="mb-3">
                     <Button type="submit">Sign In</Button>
