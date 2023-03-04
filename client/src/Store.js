ffdc008997410100a93aba9dbd1595d711fb4f5b
lintaocui <cuilintao1@163.com>
2023-03-03 18:21:46 -0800

    Fixed signout error

--- client/src/Store.js
+++ client/src/Store.js
@@ -37,7 +37,9 @@ function reducer(state, action) {
       return { ...state, cart: { ...state.cart, cartItems } };
     }
     case "USER_SIGNIN":
-      return{...state, userInfo: action.payload}
+      return{...state, userInfo: action.payload};
+    case 'USER_SIGNOUT':
+      return {...state, userInfo: null,};
     default:
       return state;
   }
