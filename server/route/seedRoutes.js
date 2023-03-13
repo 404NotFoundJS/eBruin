8b41e4b5fa4648962f1fb18fe0852629c2956e76
HarryPlier <lhuang02@g.ucla.edu>
2023-03-12 23:26:25 -0700

    Add client post request for product upload
    added HTTP POST method in seedRoutes.js for client to be able to
    post product information onto the server and saved in the
    database.
    Need to be connected with frontend product upload page.

--- server/route/seedRoutes.js
+++ server/route/seedRoutes.js
@@ -1,7 +1,9 @@
+import expressAsyncHandler from 'express-async-handler';
 import express from 'express';
 import Product from '../models/productModel.js';
 import User from '../models/userModel.js';
 import data from '../data.js';
+
 
 const seedRouter = express.Router();
 
@@ -12,4 +14,25 @@ seedRouter.get('/', async (req, res) => {
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdProducts,createdUsers });
 });
+
+seedRouter.post(
+    '/upload-product', 
+    expressAsyncHandler(async (req, res) => {
+        let newProduct = new Product({
+        name: req.body.name,
+        slug: req.body.slug,
+        image: req.body.image,
+        brand: req.body.brand,
+        category: req.body.category,
+        description: req.body.description,
+        price: req.body.price,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews
+        });
+        let product = await newProduct.save()
+        res.send(product);
+    })
+);
+
 export default seedRouter;
