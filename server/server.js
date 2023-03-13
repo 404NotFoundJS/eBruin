a02e8aaa4ddd36cfaba27bf09348498f3042a704
lintaocui <cuilintao1@163.com>
2023-03-12 19:29:06 -0700

    deleted data.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import seedRouter from "./route/seedRoutes.js";
 import productRouter from "./route/productRoutes.js";
 import userRouter from "./route/userRoutes.js";
 
@@ -18,7 +16,6 @@ then(()=>{console.log('Connected to MongoDB')}).catch(err =>{console.log(err.mes
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/seed',seedRouter);
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter);
 
