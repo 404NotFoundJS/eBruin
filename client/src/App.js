ec6447d5a8fc6835b5cb6216308214423bc0c04a
Jeremy Shi <jeremysjj666@gmail.com>
2023-03-13 23:42:02 -0700

    Fix page width problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,40 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
-import Badge from 'react-bootstrap/Badge';
-import Nav from 'react-bootstrap/Nav';
 import { useContext } from 'react';
-import { Store } from './Store';
-import CartScreen from './screens/CartScreen';
-import SigninScreen from "./screens/SigninScreen";
-import SignupScreen from "./screens/SignupScreen";
+import Badge from 'react-bootstrap/Badge';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SigninScreen from './screens/SigninScreen';
+import SignupScreen from './screens/SignupScreen';
+import { Store } from './Store';
 
 function App() {
-  const { state,dispatch: ctxDispatch} = useContext(Store);
-  const { cart , userInfo} = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
 
   const signoutHandler = () => {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
+    localStorage.removeItem('cart');
   };
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <ToastContainer position="bottom-center" limit={1} />
         <header>
-          <Navbar className="navbar navbar-custom">
-            <Container>
-              <LinkContainer to="/">
-                <Navbar.Brand>eBruin</Navbar.Brand>
-              </LinkContainer>
-              <Nav className="me-auto">
+          <Navbar expand="lg" className="navbar navbar-custom">
+            <Container fluid>
+              <Navbar.Brand href="#home">eBruin</Navbar.Brand>
+              <Nav className="ms-auto">
                 <Link to="/cart" className="nav-link">
                   <i className="fas fa-shopping-cart fa-lg"></i>
                   {cart.cartItems.length > 0 && (
@@ -44,30 +44,30 @@ function App() {
                   )}
                 </Link>
                 {userInfo ? (
-                    <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
-                      <LinkContainer to="/profile">
-                        <NavDropdown.Item>User Profile Update</NavDropdown.Item>
-                      </LinkContainer>
-                      <NavDropdown.Divider />
-                      <Link
-                          className="dropdown-item"
-                          to="#signout"
-                          onClick={signoutHandler}
-                      >
-                        Sign Out
-                      </Link>
-                    </NavDropdown>
-                ) : (
-                    <Link className="nav-link" to="/signin">
-                      Sign In
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/myProfile">
+                      <NavDropdown.Item>My Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="/"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
                     </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
                 )}
               </Nav>
             </Container>
           </Navbar>
         </header>
         <main>
-          <Container className="mt-3">
+          <Container fluid="md">
             <Routes>
               <Route path="/" element={<HomeScreen />} />
               <Route path="/home" element={<HomeScreen />} />
@@ -75,6 +75,7 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/myProfile" element={<ProfileScreen />} />
             </Routes>
           </Container>
         </main>
